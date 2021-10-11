37249f072ae903828fcec9ae465fbeaf923d276d
Mate Duda <duda12mate@gmail.com>
2021-10-12 00:13:57 +0200

    Multiple base files are selectable. After adjusting the size and position of the watermark, it can be automatically merged and saved on all the selected base images.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from tkinter import *
 from tkinter import filedialog
-
+from random import randint
 from PIL import ImageTk, Image
 
 root = Tk()
 root.resizable(width=True, height=True)
-global img1, img2, dx, dy
+global img1, img2, dx, dy, base
 
 
 def change_img(image):
@@ -16,10 +16,11 @@ def change_img(image):
 
 
 def open_img():
-    global img1
-    x = filedialog.askopenfilename(title='open')
-    img1 = Image.open(x)
+    global img1, base
+    base = list(filedialog.askopenfilenames(title='open'))
+    img1 = Image.open(base[0])
     img1 = img1.resize((600, 600), Image.ANTIALIAS)
+    base.pop(0)
     change_img(img1)
 
 
@@ -27,8 +28,8 @@ def add_img():
     global img1, img2, dx, dy
     dx = 0
     dy = 0
-    y = filedialog.askopenfilename(title='open')
-    img2 = Image.open(y)
+    mark = filedialog.askopenfilename(title='open')
+    img2 = Image.open(mark)
     img2 = img2.resize((200, 200), Image.ANTIALIAS)
     img1_c = img1.copy()
     img1_c.paste(img2, (dx, dy))
@@ -86,11 +87,27 @@ def up():
 
 
 def save():
-    global img1, img2, dx, dy
+    global img1, img2, dx, dy, base
     img1_c = img1.copy()
     img1_c.paste(img2, (dx, dy))
-    img1_c.save('Watermarked.jpg')
+    img1_c.save('Watermarked_' + f'{randint(1, 10000)}' + '.jpg')
+    try:
+        img1 = Image.open(base[0])
+        base.pop(0)
+        img1 = img1.resize((600, 600), Image.ANTIALIAS)
+        img1_c = img1.copy()
+        img1_c.paste(img2, (dx,dy))
+        change_img(img1_c)
+        return True
+    except IndexError:
+        white_img = Image.open("image.png")
+        change_img(white_img)
+        return False
 
+def save_all():
+    t = True
+    while t:
+        t = save()
 
 panel = Label(root)
 btn1 = Button(root, text='open image', command=open_img).grid(row=0, column=0)
@@ -101,6 +118,6 @@ btn_left = Button(root, text="Left", command=left).grid(row=0, column=2)
 btn_right = Button(root, text="Right", command=right).grid(row=1, column=2)
 btn_up = Button(root, text="Up", command=up).grid(row=0, column=3)
 btn_down = Button(root, text="Down", command=down).grid(row=1, column=3)
-btn_save = Button(root, text="Save", command=save).grid(row=3, column=1, columnspan=2)
-
+btn_save = Button(root, text="Save", command=save).grid(row=3, column=1)
+btn_save_all = Button(root, text="Save all", command=save_all).grid(row=3, column=2)
 root.mainloop()
